28888a5aaca82e0f2ff1c01cde3fb852e8408bf9
unipi-projects <76707150+unipi-projects@users.noreply.github.com>
2021-05-29 01:39:11 +0300

    Add files via upload

---
+++ C/BasicOperations/operations.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void main(void)
+{
+    int num1 = 5;
+    int num2 = 3;
+
+    int result_add = num1 + num2;	// calculate the result of num1 + num2
+
+    printf("result_add = %d\n", result_add);	// print the result of the addition
+
+    int num3 = 6;
+    int num4 = 4;
+
+    int result_sub = num3 - num4;	// calculate the result of num3 - num4
+
+    printf("result_sub = %d\n", result_sub);	// print the result of the subtraction        
+
+    int num5 = 10;
+    int num6 = 7;
+
+    int result_multi = num5 * num6;	// calculate the result of num5 * num6
+
+    printf("result_multi = %d\n", result_multi);	// print the result of the multiplication   
+
+    int num7 = 5;
+    float num8 = 2;
+
+    float result_div = num7 / num8;	// calculate the result of num7 / num8
+
+    printf("result_div = %f\n", result_div);	// print the result of the division
+
+    int num9 = 5;
+    int num10 = 2;
+
+    int result_mod = num9 % num10;	// calculate the result of num9 % num10
+
+    printf("result_mod = %d\n", result_mod);	// print the result of the modulo        
+}
---
+++ C/Types/types.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+
+void main(void)
+{
+    int a = 5;		    // a is an integer
+    float b = 22.1;  	// b is a floating-point number
+    char c = 'r';	    // c is a character	
+        
+    printf("a =  %d\n", a);
+    printf("b =  %f\n", b);
+    printf("c =  %c\n", c);
+}
